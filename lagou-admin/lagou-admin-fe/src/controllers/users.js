4fbe03d17ab5dde72dd4bede460af2562d62b26e
李某人氏 <1987368180@qq.com>
2020-01-11 16:59:38 +0800

    修复登录后的token后续判断

--- lagou-admin/lagou-admin-fe/src/controllers/users.js
+++ lagou-admin/lagou-admin-fe/src/controllers/users.js
@@ -1,5 +1,95 @@
+import store from 'store'
 import userTpl from '../views/user.html'
 import oAuth from '../utils/oAuth'
+
+// class Users {
+//   constructor() {
+//     this.render()
+//     this.type = ''
+//     this.isSignin = false
+//     this.username = ''
+//   }
+
+//   async render() {
+//     // 如果调用了一个async 方法，方法里如果有await
+//     // 在调用的时候必须await
+//     await this.auth()
+
+//     let that = this
+
+//     let html = userTpl({
+//       isSignin: this.isSignin,
+//       username: this.username
+//     })
+//     $('#nav').html(html)
+
+//     // 注册登录按钮点击
+//     $('#btn-signin, #btn-signup').on('click', function() {
+//       that.type = $(this).attr('id')
+//     })
+
+//     // 提交
+//     $('#btn-submit').on('click', this.handleSubmit.bind(this))
+
+//     // 注销
+//     $('body').off('click').on('click', '#btn-signout', async () => {
+//       let result = await oAuth.get({
+//         url: '/api/users/signout'
+//       })
+//       if (result.ret) {
+//         location.reload()
+//       }
+
+//       store.remove('token')
+//       location.reload()
+//     })
+//   }
+
+//   async auth() {
+//     let result = await oAuth.get({
+//       url: '/api/users/isSignin'
+//     })
+
+//     let username = result.data.username
+//     this.isSignin = username ? true : false
+//     this.username = username
+//   }
+
+//   async handleSubmit() {
+//     let data = $('.form-horizontal').serialize()
+
+//     let result = await oAuth.get({
+//       // this.type 存储了用户点了“登录”或“注册”按钮
+//       url: '/api/users/' + (this.type === 'btn-signin' ? 'signin' : 'signup'),
+//       data,
+//       type: 'POST'
+//     })
+
+//     this.handleSubmitSucc(result)
+//   }
+
+//   handleSubmitSucc(result) {
+//     $('.form-horizontal')[0].reset()
+
+//     if (result.ret) {
+//       let html = userTpl({
+//         isSignin: true,
+//         username: result.data.username
+//       })
+
+//       if (this.type === 'btn-signin') {
+//         $('#nav').html(html)
+//       } else {
+//         alert(result.data.message)
+//       }
+//     } else {
+//       alert(result.data.message)
+//     }
+
+//   }
+// }
+
+// export default new Users()
 
 class Users {
   constructor() {
@@ -32,7 +122,9 @@ class Users {
     // 标签默认事件
 
     $('.user-menu').on('click', '#signout', () => {
-      localStorage.removeItem('token')
+      // localStorage.removeItem('token')
+      // location.reload()
+      store.remove('token')
       location.reload()
     })
 
